3e9a23d041bb6f0d1942059d7588ec9b472215a1
taoonehacker@gmail.com <taoonehacker@gmail.com>
2021-02-12 06:53:24 +0800

    first commit

---
+++ Tao.Project.Test/Tao.Project.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tao.Project.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
